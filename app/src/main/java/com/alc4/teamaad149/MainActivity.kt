50718080fad745cb11c4c1a7809e910bbbc89a8c
Anaeligbo Joseph <summernet90@gmail.com>
2019-10-18 16:30:20 +0100

    Initial commit to master

---
+++ app/src/main/java/com/alc4/teamaad149/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.alc4.teamaad149
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
